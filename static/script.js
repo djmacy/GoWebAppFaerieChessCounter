7521c8a0b9356646ade09ba6680fe0bb306f4aa2
David Macy <dmacy@carroll.edu>
2023-11-14 19:39:30 -0700

    No it saves however it does not reload the calculation when it returns to the selections page yet

--- static/script.js
+++ static/script.js
@@ -52,5 +52,56 @@ function onLoad() {
     document.getElementById('thief').addEventListener('change', updateRank2Pieces);
     document.getElementById('tower').addEventListener('change', updateRank2Pieces);
 }
-//This will ensure js is run after returning from result page
-onLoad();
+
+function saveSelections() {
+    localStorage.setItem('pawn', document.getElementById('pawn').value);
+    localStorage.setItem('peasant', document.getElementById('peasant').value);
+    localStorage.setItem('soldier', document.getElementById('soldier').value);
+    localStorage.setItem('rook', document.getElementById('rook').value);
+    localStorage.setItem('bishop', document.getElementById('bishop').value);
+    localStorage.setItem('knight', document.getElementById('knight').value);
+    localStorage.setItem('catapult', document.getElementById('catapult').value);
+    localStorage.setItem('courtesan', document.getElementById('courtesan').value);
+    localStorage.setItem('chamberlain', document.getElementById('chamberlain').value);
+    localStorage.setItem('herald', document.getElementById('herald').value);
+    localStorage.setItem('inquisitor', document.getElementById('inquisitor').value);
+    localStorage.setItem('lancer', document.getElementById('lancer').value);
+    localStorage.setItem('pontiff', document.getElementById('pontiff').value);
+    localStorage.setItem('thief', document.getElementById('thief').value);
+    localStorage.setItem('tower', document.getElementById('tower').value);
+    localStorage.setItem('king', document.getElementById('king').value);
+    localStorage.setItem('queen', document.getElementById('queen').value);
+}
+
+function loadSelections() {
+    document.getElementById('pawn').value = localStorage.getItem('pawn') || '4';
+    document.getElementById('peasant').value = localStorage.getItem('peasant') || '0';
+    document.getElementById('soldier').value = localStorage.getItem('soldier') || '0';
+    document.getElementById('rook').value = localStorage.getItem('rook') || '0';
+    document.getElementById('bishop').value = localStorage.getItem('bishop') || '0';
+    document.getElementById('catapult').value = localStorage.getItem('catapult') || '0';
+    document.getElementById('courtesan').value = localStorage.getItem('courtesan') || '0';
+    document.getElementById('chamberlain').value = localStorage.getItem('chamberlain') || '0';
+    document.getElementById('herald').value = localStorage.getItem('herald') || '0';
+    document.getElementById('inquisitor').value = localStorage.getItem('inquisitor') || '0';
+    document.getElementById('lancer').value = localStorage.getItem('lancer') || '0';
+    document.getElementById('pontiff').value = localStorage.getItem('pontiff') || '0';
+    document.getElementById('thief').value = localStorage.getItem('thief') || '0';
+    document.getElementById('tower').value = localStorage.getItem('tower') || '0';
+    document.getElementById('king').value = localStorage.getItem('king') || 'King';
+    document.getElementById('queen').value = localStorage.getItem('queen') || 'Queen';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
